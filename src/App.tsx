e9aeb6a3bb96f7dd31b660640eaef087429ac7d2
Sai Dhulipalla <dhulipal@adobe.com>
2023-10-29 17:37:39 -0700

    Untrack files in .gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,31 @@
-import React, { ReactElement } from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Welcome from './components/Header';
 
-function App(): ReactElement {
+const url = "https://consensys-93520259f634.herokuapp.com/api/createRoom?user=frd";
+
+function App() {
+  // const { result } = useCreateRoom('sai');
+  const [data, setData] = React.useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+         const response = await fetch(url)
+          const newData = await response.json()
+          setData(newData)
+      };
+
+      fetchData();
+  }, [])   
+  
   return (
     <>
      <div className="App">
       <header className="App-header">
         <Welcome name="Sai"/>
+        <p>
+          {data}
+        </p>
       </header>
     </div>
     </>
